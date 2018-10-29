2c2be7a72d5b9acbcd28dea88f913aae4a1164aa
bsabljica1 <bsabljica1@etf.unsa.ba>
2018-10-29 17:24:24 +0100

    zADATAK 3

--- src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -2,10 +2,13 @@ package ba.unsa.etf.rpr.tutorijal03;
 
 public class TelefonskiBroj {
 
-    
+
     public abstract String Ispisi () {
         String s=new String();
         System.out.println();
-
     }
+
+
+
+
 }
