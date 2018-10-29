3ccd6c0869b5646024e20cbc65dbf8e4707d240e
bsabljica1 <bsabljica1@etf.unsa.ba>
2018-10-29 18:13:08 +0100

    zADATAK 3

--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -1,4 +1,12 @@
 package ba.unsa.etf.rpr.tutorijal03;
+Import java.util,HashMap;
 
 public class Imenik {
+    private String ime;
+    private TelefonskiBroj br;
+
+
+
+
+    public void dodaj(String ime, TelefonskiBroj broj)
 }
--- src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
@@ -1,4 +1,12 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
 public class MedunarodniBroj {
+    private String drz;
+    private String br;
+
+
+    MedunarodniBroj(String drzava, String broj) {
+        br=broj;
+        drz=drzava;
+    }
 }
